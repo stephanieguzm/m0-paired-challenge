7713efc57a37685350384d6fa88833e933ed18c7
Stephanie Guzman <sguzman247@gmail.com>
2022-03-31 20:48:44 -0600

    Update files with required information

---
+++ challenge/bookTitles.js
@@ -0,0 +1,9 @@
+//This is a file that contains titles of books
+var bookTitle1 = "Random Book" //String "Random Book" was assigned to the variable bookTitle1
+var pageNumber = 4 //The Number 4 was assigned to the variable pageNumber
+var publisher = "Penguin House"
+var hasBook = true //The Boolean true was assigned to hasBook
+var quantity = 9001 //It's over 9000
+var libraryBranch = "Ross Broadway DPL"
+
+var pageNumbers = [2, 6, 12, 22] //The Array pageNumbers has 4 elements which are all integers
---
+++ challenge/index.md
@@ -0,0 +1,12 @@
+# Book Titles
+1. Race After Technology
+2. The Nightingale
+3. The Body Keeps the Score
+4. Harry Potter and the Chamber of Secrets
+5. Behold the Dreamers
+
+## Workflow Reflections
+1. Driver: Stephanie Guzman
+2. Navigators: Anthony Shellman and Abby Luce
+- Anthony and Abby took turns navigating
+3. After forking the original file, Abby double checked her notes for the git clone command
